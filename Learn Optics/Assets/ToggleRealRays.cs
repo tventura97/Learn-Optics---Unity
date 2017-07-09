cbab7c49494ac2fbe4bd1046365190ce97690230
Tristan Ventura <tventura0297@gmail.com>
2017-07-08 21:18:29 -0400

    Added Virtual and Real Ray Toggles

---
+++ Learn Optics/Assets/ToggleRealRays.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleRealRays : MonoBehaviour {
+
+    GameObject[] RealRays;
+
+	void Start () {
+        RealRays = GameObject.FindGameObjectsWithTag("RealRay");
+	}
+	
+	public void OnToggle () {
+		for (int i = 0; i < RealRays.Length; i++)
+        {
+            RealRays[i].SetActive(!RealRays[i].activeSelf);
+        }
+	}
+}
